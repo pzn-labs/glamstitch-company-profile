5a9521d95f466580b80c82ee51f22f5c4f5bfa83
dwiwahyufauzan <dwiwhy31@gmail.com>
2025-10-02 11:40:27 +0700

    Updates

--- _app/immutable/chunks/BmVZP6dj.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_47b9to)==null?void 0:s.base)??"/glamstitch-company-profile";var t;const e=((t=globalThis.__sveltekit_47b9to)==null?void 0:t.assets)??a;export{e as a,a as b};
