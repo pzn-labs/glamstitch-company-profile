33e49cc1a095ad8c35dd29dd3f4affe9334e2e3e
dwiwahyufauzan <dwiwhy31@gmail.com>
2025-10-02 12:45:46 +0700

    Updates

---
+++ _app/immutable/chunks/2h_Ke4VJ.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_ju5nbk)==null?void 0:s.base)??"/glamstitch-company-profile";var a;const e=((a=globalThis.__sveltekit_ju5nbk)==null?void 0:a.assets)??t??"";export{e as a,t as b};
--- _app/immutable/entry/start.BHGINPRB.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/nU9N3W3e.js";export{o as load_css,r as start};
