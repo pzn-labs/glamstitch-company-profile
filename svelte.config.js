d97c5bf69c8e96f539caacfa071792a5e5aeee62
dwiwahyufauzan <dwiwhy31@gmail.com>
2025-10-02 09:18:25 +0700

    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
-import path from 'path';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+const dev = process.argv.includes('dev');
+
+export default {
+  preprocess: vitePreprocess(),
+
   kit: {
+    adapter: adapter(),
     paths: {
-      base: '/glamstitch-company-profile',
+      // nama repo kamu
+      base: dev ? '' : '/glamstitch-company-profile'
     },
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html',
-      precompress: false,
-      strict: true
-    }),
-  },
+    alias: {
+      $components: 'src/components',
+      $lib: 'src/lib'
+    }
+  }
 };
-
-export default config;
